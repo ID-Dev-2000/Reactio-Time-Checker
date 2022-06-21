19bbb8f6d7612cea7f11d1bbbd12e91542e7c7f1
ID <izaakduncan@outlook.com>
2022-06-20 23:12:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const speedBox = document.getElementById('speedBox')
+const testButton = document.getElementById('testButton')
+const resultDisplay = document.getElementById('resultDisplay')
+
+let timeValue1 = 0
+let timeValue2 = 0
+let randomTimeValue = 0
+let checkValue = 0
+
+function speedBoxEventListener() {
+    timeValue2 = Date.now()
+    speedBox.style.backgroundColor = 'White'
+    resultDisplay.innerHTML = timeValue2 - timeValue1
+}
+
+function handleReactionSpeed() {
+    randomTimeValue = Math.floor(Math.random() * 1200)
+    setTimeout(function() {
+        timeValue1 = Date.now()
+        speedBox.style.backgroundColor = 'Red'
+        checkValue++
+        if (checkValue == 1) {
+        speedBox.addEventListener('click', speedBoxEventListener)
+        checkValue = 0
+        }
+    }, randomTimeValue)
+}
+
+testButton.addEventListener('click', function() {
+    speedBox.style.backgroundColor = 'Blue'
+    setTimeout(handleReactionSpeed, 400)
+})
